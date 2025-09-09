e22e94030e835934baddc8edcc3ba8d96d4e77e9
Alfons Wallin <144wallfin@gmail.com>
2025-09-09 08:41:43 +0200

    Added door button that only activates if the assigned player walks on it.

---
+++ Assets/Scripts/DoorScripts/DoorButtonSpecificPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorButtonSpecificPlayer : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    List<MonoBehaviour> doorToOpen = new();
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(player.tag))
+        {
+            foreach (var door in doorToOpen)
+            {
+                door.enabled = true;
+            }
+        }
+    }
+}
